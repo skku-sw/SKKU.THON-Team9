80e3fbefe44da6bd618b388c562d709327031ed6
SincerityHun <mark.seonghun.jung@gmail.com>
2023-09-24 02:45:22 +0900

    feat: add CORS options in flask server

--- Backend/app.py
+++ Backend/app.py
@@ -4,6 +4,7 @@ from db_model import initialize_db
 from api_way_user import user_api
 from api_way_pdf import pdf_api
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 app = Flask(__name__)
 
@@ -20,5 +21,8 @@ db_session = initialize_db(app)
 app.register_blueprint(user_api, url_prefix="/user")
 app.register_blueprint(pdf_api, url_prefix="/pdf")
 
+CORS(app)
+
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(debug=True)
