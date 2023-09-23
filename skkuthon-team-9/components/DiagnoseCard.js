76dec41efe0ad58cec8ddeb829190e8674346cec
Suhyeon <fbtngus2789@naver.com>
2023-09-24 07:49:25 +0900

    최종 수정

--- skkuthon-team-9/components/DiagnoseCard.js
+++ skkuthon-team-9/components/DiagnoseCard.js
@@ -1,19 +1,28 @@
+import { useRouter } from "next/router";
 import Image from "next/image";
 
 import styles from "./DiagnoseCard.module.css";
 
 const DiagnoseCard = ({ diagnosis }) => {
+  const router = useRouter();
+
   const date = diagnosis.hospitalization_dates.split(" 00:")[0];
+
+  const onPDFClick = () => {
+    router.push(diagnosis.filename);
+  };
 
   return (
     <div>
-      <Image
-        layout="responsive"
-        src={"/images/pdf-logo.png"}
-        alt="pdf image"
-        width={100}
-        height={100}
-      />
+      <div className={styles.pdf} onClick={onPDFClick}>
+        <Image
+          layout="responsive"
+          src={"/images/pdf-logo.png"}
+          alt="pdf image"
+          width={100}
+          height={100}
+        />
+      </div>
       <div className={styles.infoWrapper}>
         <span>{diagnosis.medical_institution}</span>
         <span>{date}</span>
